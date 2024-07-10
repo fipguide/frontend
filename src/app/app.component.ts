04356e51fc50c915f6ea9886a0541ef6f9d2b191
lennartrommeiss <lennart.rommeiss@deutschebahn.com>
2024-07-10 16:05:13 +0200

    fix: first setup
    Signed-off-by: lennartrommeiss <lennart.rommeiss@deutschebahn.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as fipdata from '../assets/json/out.json';
+import { Company, CompanyComponent } from './company/company.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CompanyComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'fipguide';
+  selectedCompany: Company | undefined;
+
+  public getdatafromJson(countryAbbreviation: string): Company {
+    return (
+      fipdata.countries[0].companys.find(
+        (i) => i.abbreviation === countryAbbreviation
+      ) || {
+        abbreviation: '',
+        name: '',
+        generalInformation: '',
+      }
+    );
+  }
+
+  public setSelectedCompany(countryAbbreviation: string) {
+    this.selectedCompany = this.getdatafromJson(countryAbbreviation);
+  }
 }
